a4e5dec7c2d53ca9211a5ee04c73771d81638a2b
kirillzholudev <kirillzholudev24@gmail.com>
2022-10-21 11:21:55 +0200

    add tests

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Product, Payment, OrderItem, Order
+
+admin.site.register(Product)
+admin.site.register(Payment)
+admin.site.register(OrderItem)
+admin.site.register(Order)
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from shop.models import Product, Payment, OrderItem, Order
 
-# Create your tests here.
+
+class TestDataBase(TestCase):
+    fixtures = [
+        "shop/fixtures/data.json"
+    ]
+
+    def setUp(self):
+        self.user = User.objects.get(username='admin')
+
+    def test_user_exists(self):
+        users = User.objects.all()
+        users_number = users.count()
+        user = users.first()
+        self.assertEqual(users_number, 1)
+        self.assertEqual(user.username, 'admin')
+        self.assertTrue(user.is_superuser)
+
+    def test_user_check_password(self):
+       self.assertTrue(self.user.check_password('991206'))
+
+    def test_all_data(self):
+        self.assertGreater(Product.objects.all().count(), 0)
